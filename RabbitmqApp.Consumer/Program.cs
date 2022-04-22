0e2d22aa41ae404a3b6b6e8361609f9c7d9b8d30
senoldinc <senol.dinc@windowslive.com>
2022-04-23 00:18:12 +0300

    first commit

---
+++ RabbitmqApp.Consumer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory
+        {
+            HostName = "localhost",
+            UserName = "guest",
+            Password = "guest"
+        };
+
+        var connection = factory.CreateConnection();
+
+        var channel = connection.CreateModel();
+        channel.QueueDeclare(
+               queue: "customer",
+               durable: false,
+               exclusive: false,
+               autoDelete: false,
+               arguments: null
+           );
+        
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (model, ea) => {
+            var body = ea.Body.ToArray();
+            var data = Encoding.UTF8.GetString(body);
+            Console.WriteLine($"Getting customer: {data}");
+        };
+
+        channel.BasicConsume(queue: "customer", autoAck: true, consumer: consumer);
+
+        Console.ReadLine();
+    }
+}
---
+++ RabbitmqApp.Producer/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+
+class Program
+{
+    static Task Main(string[] args)
+    {
+        var factory = new ConnectionFactory
+        {
+            HostName = "localhost",
+            UserName = "guest",
+            Password = "guest"
+        };
+
+        var connection = factory.CreateConnection();
+
+        var x = string.Empty;
+
+        while (x != "X")
+        {
+            Console.WriteLine("Press any key to continue");
+            x = Console.ReadLine()?.ToUpperInvariant();
+
+            if (x != "X")
+            {
+                Console.Write("Customer Name: ");
+                var name = Console.ReadLine();
+                Console.Write("Email: ");
+                var email = Console.ReadLine();
+
+                var customer = new
+                {
+                    id = Guid.NewGuid().ToString(),
+                    name = name,
+                    email = email
+                };
+
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: "customer",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null
+                    );
+
+                    var customerPayload = JsonSerializer.Serialize(customer);
+                    var body = Encoding.UTF8.GetBytes(customerPayload);
+
+                    channel.BasicPublish(
+                        exchange: "",
+                        routingKey: "customer",
+                        basicProperties: null,
+                        body: body
+                    );
+                }
+            }
+        }
+
+        return Task.CompletedTask;
+    }
+}
+
